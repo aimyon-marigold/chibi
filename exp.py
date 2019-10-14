324c796339633d4219e60ee755c34d151ac7e587
INOUE Misato <21716014@jwu.ac.jp>
2019-10-14 16:51:04 +0900

    exp

--- exp.py
+++ exp.py
@@ -1,5 +1,8 @@
 
-class Val(object):
+class Expr(object):
+    pass
+
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
@@ -12,7 +15,50 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+assert isinstance(v, Expr)  # ==> True
+assert isinstance(v, Val)   # ==> True
+assert isinstance(v, int)   # ==> Folse
 
+def toExpr(a):
+    if not isinstance(a, Expr):
+            a = Val(a)
+    return a
+
+class Binary(Expr)
+
+class Add(Binary):
+    __slots__ = ['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   #aとbは式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(1, Add(1, 2))
+print(e.eval())
+assert e.eval() == 4
+
+e = Add(Val(1), Add(Val(2),Val(3)))
+assert e.eval() == 6
+
+class Sub(Binary):
+    __slots__ = ['left', 'right']
+    def __init__(self, a, b):
+        self.left = toExpr(a)   #aとbは式
+        self.right = toExpr(b)
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+class Div(Binary):
+        __slots__ = ['left', 'right']
+        def __init__(self, a, b):
+            self.left = toExpr(a)   #aとbは式
+            self.right = toExpr(b)
+        def eval(self):
+            return self.left.eval() + self.right.eval()
+
+#次のステップ
 
 print()
 print()
+
