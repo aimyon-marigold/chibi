2c20b421f4b3b98e5400d7c809cdd107b9880208
INOUE Misato <21716014@jwu.ac.jp>
2019-10-28 16:50:47 +0900

    parser

--- parser.py
+++ parser.py
@@ -1,32 +1,29 @@
-from exp import Val, Add
+from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
-    num = int(s)
-    return Val(num)
-
-'''
-e = parse("123")
-print(e)
-
-s = "123+456"
-pos = s.find('+')   # + 記号を探す
-print('pos', pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s1, s2)       # + 記号で分割
-'''
-
-def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else:
+    if s.find('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1), parse(s2))
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    if s.find('/') > 0:
+        pos = s.find('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1), parse(s2))
+    return Val(int(s))
 
-e = parse("123+456+789")
-print(e)
-
+e = parse("1-2-3")
+print(e, e.eval())  # -4
+assert e.eval() == -4
+ 
