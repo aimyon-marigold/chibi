39d437829f9d517fe18ca85d20f40e709d854d4c
INOUE Misato <21716014@jwu.ac.jp>
2020-01-20 16:33:28 +0900

    abc14

---
+++ practice.py
@@ -0,0 +1,9 @@
+w = int（input（））
+s = [範囲内のiのi（w + 1）]
+n = int（input（））
+範囲（n）のiの場合：
+    a = list（map（int、input（）。split（ '、'）））
+    s [a [0]]、s [a [1]] = s [a [1]]、s [a [0]]
+
+範囲（1、w + 1）のiの場合：
+    print（s [i]）
