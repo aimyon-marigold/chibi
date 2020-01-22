0900c894611388298a978d2840e640b8812647aa
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:57:13 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,5 +1,8 @@
-while 1:
-    b,r,g,c,s,t=map(int,input().split())
-    if [b,r,g,c,s,t].count(0)==6:break
-    print(100+(b+r)*15+g*7+c*2+(b*5+r*3)*13-(t-(s+b*5+r*3))*3)
-    
+while True:
+  b, r, g, c, s, t = map(int, input().split())
+  if b + r + g + c + s + t == 0:
+    break
+  normal_game = t - b * 5  - r * 3
+  bonus_game  = b * 5 + r * 3
+  print(100 + bonus_game * 16 + 15 * b + 15 * r + 7 * g + 2 * c + 3 * s - t * 3)
+  
