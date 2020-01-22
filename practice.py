038ca7879398ebb9a5e141fce04e9aae161793bf
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:12:14 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
-lst = sorted(list(map(int, input().split())))
-if lst[0] == lst[1] and lst[2] == lst[3]:
-  print("yes")
-else:
-  print("no")
-  
+a=[0]*100
+b=list(map(int,input().split()))
+for i in range(12):
+    a[b[i]-1]+=1
+c=0
+for i in range(100):
+    if a[i] and a[i]%4==0: c+=a[i]//4
+print('yes' if c==3 else 'no')
