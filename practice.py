91d2c4f450cf5ad270331ce20cd236a4b8f35a20
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:13:02 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,31 +1,20 @@
-def put_class(n):
-    if n <= 48:
-      print("light fly")
-    elif n <= 51:
-      print("fly")
-    elif n <= 54:
-      print("bantam")
-    elif n <= 57:
-      print("feather")
-    elif n <= 60:
-      print("light")
-    elif n <= 64:
-      print("light welter")
-    elif n <= 69:
-      print("welter")
-    elif n <= 75:
-      print("light middle")
-    elif n <= 81:
-      print("middle")
-    elif n <= 91:
-      print("light heavy")
-    else:
-      print("heavy")
-  
-  while True:
-    try:
-      put_class(float(input()))
-    except EOFError:
-      break
-  
-    
+import sys
+import os
+import math
+import itertools
+
+d = {}
+d['A'] = 0
+d['B'] = 0
+d['AB'] = 0
+d['O'] = 0
+
+for s in sys.stdin:
+    s = s.strip()
+    type = s.split(',')[-1]
+    d[type] += 1
+
+print(d['A'])
+print(d['B'])
+print(d['AB'])
+print(d['O'])
