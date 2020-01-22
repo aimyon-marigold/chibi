b8985046451d54eb514987dd19757053bf7e929e
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:02:25 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,1 +1,16 @@
-print(max(sum(map(int,input().split())),sum(map(int,input().split()))))
+n = int(input())
+m = int(input())
+lst = [i + 1 for i in range(2 * n)]
+for _ in range(m):
+  k = int(input())
+  if k == 0:
+    lst1 = lst[0:n]
+    lst2 = lst[n:2 * n]
+    lst = [lst2[i//2] if i % 2 else lst1[i//2] for i in range(n * 2)]
+  else:
+    lst1 = lst[0:k]
+    lst2 = lst[k:2 * n]
+    lst = lst2 + lst1
+for i in lst:
+  print(i)
+  
