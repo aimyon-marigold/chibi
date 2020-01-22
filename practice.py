93cc466976ff4ccdea1596bd29cf723ad9674acd
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:22:00 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,30 +1,11 @@
-def get_input():
-    while True:
-        try:
-            yield ''.join(input())
-        except EOFError:
-            break
-
-N = list(get_input())
-for l in range(len(N)):
-    cards = [1 for i in range(11)]
-    cards[0] = 0
-    C1,C2,C3 = [int(i) for i in N[l].split()]
-    cards[C1] = 0
-    cards[C2] = 0
-    cards[C3] = 0
-
-    point = C1+C2
-    ok = 0
-    ng = 0
-
-    for i in range(1,11):
-        if cards[i] == 1 and C1+C2+i <= 20:
-            ok += 1
-        elif cards[i] == 1:
-            ng += 1
-
-    if ok >= ng:
-        print("YES")
+while True:
+  try:
+    c1, c2, c3 = map(int, input().split())
+    lst = [i for i in range(1,11) if i not in (c1, c2, c3) and i + c1 + c2 <= 20]
+    if len(lst) >= 4:
+      print("YES")
     else:
-        print("NO")
+      print("NO")
+
+  except EOFError:
+    break
