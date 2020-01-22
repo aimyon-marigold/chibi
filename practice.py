93f83ac5725f69e5bdddb87491ddc9a8ac120c70
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:41:44 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,7 +1,2 @@
-import sys
-from math import*
-for e in sys.stdin:
- a,b,c,d,x,y=map(float,e.split(','))
- g=2*atan2(d-b,c-a);s,t=sin(g),cos(g);x-=a;y-=b
- print(f'{t*x+s*y+a:.6f} {s*x-t*y+b:.6f}')
- 
+a, b = map(int, input().split())
+print(a * b / 3.305785)
