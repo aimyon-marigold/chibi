432181101174f99d5b95847934c987d79bbf6ca5
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:04:09 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,16 +1,9 @@
-n = int(input())
-m = int(input())
-lst = [i + 1 for i in range(2 * n)]
-for _ in range(m):
-  k = int(input())
-  if k == 0:
-    lst1 = lst[0:n]
-    lst2 = lst[n:2 * n]
-    lst = [lst2[i//2] if i % 2 else lst1[i//2] for i in range(n * 2)]
-  else:
-    lst1 = lst[0:k]
-    lst2 = lst[k:2 * n]
-    lst = lst2 + lst1
-for i in lst:
-  print(i)
-  
+n=int(input())
+s=[i for i in range(1,2*n+1)]
+for _ in range(int(input())):
+    a=int(input())
+    if a:
+        s=s[a:]+s[:a]
+    else: 
+        s=[s[i+n*j]for i in range(n) for j in[0,1]]
+print(*s,sep='\n')
