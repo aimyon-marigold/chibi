480869838f1bdc2718ec90499d2a0d67013ad8ff
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:02:34 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,9 +1,13 @@
-n=[0]*101
-while 1:
-    try:n[int(input())]+=1
-    except:break
-m_n=max(n)
-for i in range(n.count(m_n)):
-    print(n.index(m_n)+i)
-    n.remove(m_n)
+high_max=0
+high_low=1000000
+while True:
+    try:
+        N=float(input())
+        if N>high_max:high_max=N
+        elif N<high_low:high_low=N
+    except:
+        ans=high_max-high_low
+        print("{:.1f}".format(ans))
+        break
+    
     
