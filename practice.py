dafaa14396a512d71a28b9ce2a2c3e20c7faa658
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:13:58 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,8 +1,6 @@
-a=[0]*100
-b=list(map(int,input().split()))
-for i in range(12):
-    a[b[i]-1]+=1
-c=0
-for i in range(100):
-    if a[i] and a[i]%4==0: c+=a[i]//4
-print('yes' if c==3 else 'no')
+h, a, b = map(int, input().split())
+ans = 0
+for i in range(a, b + 1):
+  if h % i == 0:
+    ans += 1
+print(ans)
