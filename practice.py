6d8a83682c321693d9c9d8140c3ed68f4275be25
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:09:29 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,29 +1,31 @@
-while True:
+def put_class(n):
+    if n <= 48:
+      print("light fly")
+    elif n <= 51:
+      print("fly")
+    elif n <= 54:
+      print("bantam")
+    elif n <= 57:
+      print("feather")
+    elif n <= 60:
+      print("light")
+    elif n <= 64:
+      print("light welter")
+    elif n <= 69:
+      print("welter")
+    elif n <= 75:
+      print("light middle")
+    elif n <= 81:
+      print("middle")
+    elif n <= 91:
+      print("light heavy")
+    else:
+      print("heavy")
+  
+  while True:
     try:
-        W = float(input())
-        if W <= 48:
-            print("light fly")
-        elif W <= 51:
-            print("fly")
-        elif W <= 54:
-            print("bantam")
-        elif W <= 57:
-            print("feather")
-        elif W <= 60:
-            print("light")
-        elif W <= 64:
-            print("light welter")
-        elif W <= 69:
-            print("welter")
-        elif W <= 75:
-            print("light middle")
-        elif W <= 81:
-            print("middle")
-        elif W <= 91:
-            print("light heavy")
-        else:
-            print("heavy")
+      put_class(float(input()))
     except EOFError:
-        break
-    
+      break
+  
     
