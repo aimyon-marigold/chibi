754c60d8ed288111952a84a54f893b05b8a0a16a
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:45:45 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,20 +1,12 @@
-n=int(input())
-for i in range(n):
-    runner=0
-    out=0
-    score=0
-    while 1:
-        state=input()
-        if state=="HIT":
-            if runner<3:
-                runner+=1
-            else:
-                score+=1
-        elif state=="HOMERUN":
-            score+=runner+1
-            runner=0
-        else:
-            out+=1
-            if out==3:break
-    print(score)
+while True:
+    try:
+        _= int(input())
+    except:
+        break
+    t= 0
+    ans= []
+    for v in sorted(list(map(int, input().split()))):
+        t+= v
+        ans.append(t)
+    print(sum(ans))
     
