4da6a0c8ab15856c77f3473de5f9a92ccfd0e71c
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:55:36 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,7 +1,5 @@
 while 1:
-    a,b=input().split()
-    if a=='0':break
-    h=0
-    for i,j in zip(a,b):h+=i==j
-    print(h,len(set(a)&set(b))-h)
+    b,r,g,c,s,t=map(int,input().split())
+    if [b,r,g,c,s,t].count(0)==6:break
+    print(100+(b+r)*15+g*7+c*2+(b*5+r*3)*13-(t-(s+b*5+r*3))*3)
     
