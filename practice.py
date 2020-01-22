583cb5ad64007cb8410c34d91fd17b8a4c330be0
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:10:09 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,11 +1,6 @@
-def f(n,x):
-    for i in range(9):
-        if n[i] != -1 and n[i] != x[i]: return 1
-import itertools
-u=[1,2,3,4,5,6,7,8,9]
-a=0
-n=list(map(int,input().split()))
-for x in itertools.permutations(u):
-    if f(n,x):continue
-    if x[0]+x[2]+x[5]-x[8]+(x[1]+x[4]-x[7])*10+(x[3]-x[6])*100==0:a+=1
-print(a)
+lst = sorted(list(map(int, input().split())))
+if lst[0] == lst[1] and lst[2] == lst[3]:
+  print("yes")
+else:
+  print("no")
+  
