d1805172893ff8d8730ad3b69243a88b38ef88cd
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 12:18:19 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,10 +1,1 @@
-n = int(input())
-alst = list(map(int, input().split()))
-wlst = list(map(int, input().split()))
-right = [w for a, w in zip(alst, wlst) if a == 0]
-left = [w for a, w in zip(alst, wlst) if a == 1]
-if right and left:
-  print(min(right) + min(left))
-else:
-  print(0)
-  
+print(sum(list(map(int,input().split()))))
