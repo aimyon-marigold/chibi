0b5325a6e54d743f2a7495bf7ffbeb93bfc600fc
INOUE Misato <21716014@jwu.ac.jp>
2020-01-22 11:43:30 +0900

    abc14

--- practice.py
+++ practice.py
@@ -1,2 +1,20 @@
-a, b = map(int, input().split())
-print(a * b / 3.305785)
+n=int(input())
+for i in range(n):
+    runner=0
+    out=0
+    score=0
+    while 1:
+        state=input()
+        if state=="HIT":
+            if runner<3:
+                runner+=1
+            else:
+                score+=1
+        elif state=="HOMERUN":
+            score+=runner+1
+            runner=0
+        else:
+            out+=1
+            if out==3:break
+    print(score)
+    
